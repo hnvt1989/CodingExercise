6472c5ce23e7a0dde3603ca450e0762d9f5c7523
Huy <huynguyenvt1989@gmail.com>
2016-08-14 15:38:00 -0700

    initial commit

---
+++ CodingExercise/Objects.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingExercise
+{
+    public class Deck
+    {
+        private List<Card> _cards;
+        public Deck()
+        {
+
+        }
+
+        public List<Card> Cards
+        {
+            get
+            {
+                if (_cards == null)
+                    _cards = populateDeckOfCards();
+                return _cards;
+            }
+        }
+        private List<Card> populateDeckOfCards()
+        {
+            var ret = new List<Card>();
+
+            foreach (var suit in SuitAndRank.Suits)
+            {
+                foreach (var rank in SuitAndRank.Ranks)
+                {
+                    ret.Add(new Card(suit.Key, rank.Key));
+                }
+            }
+            return ret;
+        }
+
+        public List<Card> Sort(List<Card> cards)
+        {
+            cards.Sort((c1, c2) => c1.Point.CompareTo(c2.Point));
+            return cards;
+        }
+
+        public List<Card> Shuffle(List<Card> cards,int nSwap = 20)
+        {
+
+            Random rnd = new Random();
+            
+            for (int i = 0; i < nSwap; i++)
+            {
+                int r1 = rnd.Next(0, 51);
+                int r2 = -1;
+                while (r2 < 0)
+                {
+                    r2 = rnd.Next(0, 51);
+                    if (r2 == r1)
+                        r2 = -1;
+                }
+
+                var tmp = cards[r1];
+                cards[r1] = cards[r2];
+                cards[r2] = tmp;
+            }
+
+            return cards;
+
+        }
+    }
+
+    public class Card
+    {
+        private string _suit;
+        private string _rank;
+
+        public Card(string suit, string rank)
+        {
+            if (!SuitAndRank.Suits.ContainsKey(suit))
+                throw new KeyNotFoundException("Invalid suit:" + suit);
+            if (!SuitAndRank.Ranks.ContainsKey(rank))
+                throw new KeyNotFoundException("Invalid rank:" + rank);
+            _suit = suit;
+            _rank = rank;
+        }
+
+        public string Suit { get { return _suit; } }
+        public string Rank { get { return _rank; } }
+
+        public override string ToString()
+        {
+            return _suit + "[" + _rank + "]";
+        }
+
+        public int Point
+        {
+            get
+            {
+                return SuitAndRank.Suits[_suit] * 13 + SuitAndRank.Ranks[_rank];
+            }
+        }
+    }
+
+    public static class SuitAndRank
+    {
+        public static Dictionary<string, int> Suits = new Dictionary<string, int>()
+        {
+            {"Space", 1 },
+            {"Club", 2 },
+            {"Diamond", 3 },
+            {"Heart", 4 }
+
+        };
+
+        public static Dictionary<string, int> Ranks = new Dictionary<string, int>()
+        {
+            {"Two", 2 },
+            {"Three", 3 },
+            {"Four", 4 },
+            {"Five", 5 },
+            {"Six", 6 },
+            {"Seven", 7 },
+            {"Eight", 8 },
+            {"Nine", 9 },
+            {"Ten", 10 },
+            {"Jack", 11 },
+            {"Queen", 12 },
+            {"King", 13 },
+            {"Ace", 14 }
+        };
+    }
+}
---
+++ Tests/Tests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using CodingExercise;
+using System.Linq;
+
+namespace Tests
+{
+    public class Tests
+    {
+
+        [Test]
+        public void should_popluate_deck_of_cards()
+        {
+            var deck = new Deck();
+            Assert.AreEqual(52, deck.Cards.Count);
+
+            //verify that the deck of cards has no duplicate card
+            var hasDuplicate = deck.Cards.GroupBy(c => new { c.Rank, c.Suit }).Any(c1 => c1.Count() > 1);
+            Assert.AreEqual(false, hasDuplicate);
+        }
+
+        [Test]
+        public void should_throw_exception_when_suit_is_invalid()
+        {
+            Assert.Throws(Is.TypeOf<KeyNotFoundException>()
+                            .And.Message.Contains("Invalid suit:")
+                            , () => new Card("invalid", "Two"));
+        }
+
+        [Test]
+        public void should_throw_exception_when_rank_is_invalid()
+        {
+            Assert.Throws(Is.TypeOf<KeyNotFoundException>()
+                            .And.Message.Contains("Invalid rank:")
+                            , () => new Card("Space", "invalid"));
+        }
+
+        [Test]
+        public void should_sort_the_deck_of_cards_by_ascending_order()
+        {
+            var deck = new Deck();
+            var sortedDeckOfCards = deck.Sort(deck.Cards);
+            var ordered = sortedDeckOfCards.Zip(sortedDeckOfCards.Skip(1), (a, b) => new { a, b })
+                                    .All(p => p.a.Point < p.b.Point);
+            Assert.AreEqual(true, ordered);
+        }
+
+        [Test]
+        public void should_shuffle_the_deck_of_the_cards()
+        {
+            var deck = new Deck();
+
+            //shuffle the deck by swapping the card 20 times
+            var shuffledCards = deck.Shuffle(deck.Cards, 20);
+
+            //verify that the deck is no longer sorted.
+            var ordered = shuffledCards.Zip(shuffledCards.Skip(1), (a, b) => new { a, b })
+                        .All(p => p.a.Point < p.b.Point);
+            Assert.AreEqual(false, ordered);
+
+            //verify there is no duplicated card
+            var hasDuplicate = shuffledCards.GroupBy(c => new { c.Rank, c.Suit }).Any(c1 => c1.Count() > 1);
+            Assert.AreEqual(false, hasDuplicate);
+        }
+
+
+    }
+}
